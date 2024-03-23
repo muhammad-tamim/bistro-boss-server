5aa6446d694aad2ec993b9fcd679d60e9f2e1c1f
Muhammad Tamim <mdt40234@gmail.com>
2024-03-23 16:55:40 +0600

    Added reviews on http://localhost:5000/reviews this url

--- index.js
+++ index.js
@@ -26,9 +26,14 @@ async function run() {
 
 
         const menuCollection = client.db("bistro-boss-db").collection("menu")
+        const reviewsCollection = client.db("bistro-boss-db").collection("reviews")
 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
+            res.send(result)
+        })
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray();
             res.send(result)
         })
 
